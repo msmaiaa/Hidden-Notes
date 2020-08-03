6956a440fe9aaf602743edf2ac8acb0247de708b
Matheus Santos <roxx77344@gmail.com>
2020-08-03 11:52:10 -0300

    added note generator route with encryption

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 require('dotenv').config({path: "C:\\Users\\msmai\\Desktop\\code\\angular\\projeto\\hidden-notes\\server" + '/.env'})
+const { nanoid } = require("nanoid");
+const NoteModel = require('./models/NoteModel');
 const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 app.use(bodyparser());
+
 
 mongoose.connect(process.env.DB_LINK,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
@@ -15,8 +18,23 @@ app.get('/note/:id', (req,res)=>{
     res.status(200).send({message: 'Teste'})
 })
 
-app.post('/note/new',(req,res)=>{
-    res.status(200).send({message: req.body.content})
+app.post('/note/new', async(req,res)=>{
+    try{
+        if(!req.body.content){
+            return res.status(500).send({error: 'Error while trying to get message content'})
+        }
+        let newPageId = nanoid()
+        let newNote = new NoteModel({content: req.body.content, pageId:newPageId})
+    
+        newNote.content = bcrypt.hashSync(req.body.content, parseInt(process.env.BCRYPT_SALT))
+    
+        await newNote.save()
+        .then((n)=>{
+            return res.status(200).send(n);
+        })
+    }catch(err){
+        return res.status(500).send(err);
+    }
 })
 
 app.listen(3000);
--- server/models/NoteModel.js
+++ server/models/NoteModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const NoteSchema = new mongoose.Schema({
-    'content': String
+    'content': String,
+    'pageId': String,
 })
 module.exports = mongoose.model('Note',NoteSchema);
